569330beab41b86b78023ef56f6b8a35ee0cdbcc
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-11-02 05:39:07 +0800

    Create GUIMain.java

---
+++ GUIMain.java
@@ -0,0 +1,46 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class GUIMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIMain frame = new GUIMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUIMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGUIMain = new JLabel("");
+		lblGUIMain.setIcon(new ImageIcon("D:\\Users\\63916\\Downloads\\PesoSaveMain.png"));
+		lblGUIMain.setBounds(0, 0, 1280, 770);
+		contentPane.add(lblGUIMain);
+	}
+}
