b83b3083c2261c0186870a8439a8c97a0a15492e
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-11-04 08:42:00 +0800

    Update GUIMain.java

--- GUIMain.java
+++ GUIMain.java
@@ -1,4 +1,6 @@
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -31,12 +33,20 @@ public class GUIMain extends JFrame {
 	 */
 	public GUIMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setResizable(false); // Make the frame non-resizable
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+        // Calculate the center position
+        int centerX = (screenSize.width - 1280) / 2;
+        int centerY = (screenSize.height - 780) / 2;
+
+        // Set the frame's position and size
+        setBounds(centerX, centerY, 1280, 750);
+
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 		
 		JLabel lblGUIMain = new JLabel("");
 		lblGUIMain.setIcon(new ImageIcon("D:\\Users\\63916\\Downloads\\PesoSaveMain.png"));
