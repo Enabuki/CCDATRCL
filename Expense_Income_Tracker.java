3a3c3a04aaa6ca5f5da303be9ce04f48c950cfb5
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2023-11-09 06:54:07 +0800

    Create Expense_Income_Tracker.java

---
+++ Expense_Income_Tracker.java
@@ -0,0 +1,15 @@
+public class Expense_Income_Tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+        
+    }
+
+    public void setVisible(boolean b) {
+    }
+
+}
